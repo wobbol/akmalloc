d639f8417f4453e55f8c4a55fa460acaa3ac300c
Nicole Fontenot <nfontenot27@gmail.com>
2018-03-09 23:52:10 -0600

    Bugfix for `split()` and `myfree()`
    Fix `split()` - Massive unused space between allocated blocks.
    Fix `myfree()` - `struct block_t` is `size(struct block_t)` bytes back
                     from `ptr`.
    Add to `print_block()` - Now prints the first and last byte that was
    allocated in char form.
    Add `test()` - allocates fills and prints a string. Also, it prints all
    memory blocks.

--- k.c
+++ k.c
@@ -22,7 +22,7 @@ void init()
 /* size should be sizeof(*in) + bytes to allocate */
 void split(struct block_t *in, size_t size) //brake a piece of all_k_mem, like the chocolate I shouldn't have eaten last night...
 {
-	struct block_t *new = in + size;
+	struct block_t *new = (void *)in + size;
 
 	new->size = in->size - size;
 	new->free = true;
@@ -72,16 +72,17 @@ void joinFreeBlocks() //glue free blocks beside each other
 
 void myfree(void *ptr)
 {
+	struct block_t *b = ptr - sizeof(struct block_t);
 	if( // eh, don't know if I like this style
-		ptr < (void *)all_k_mem ||
-		ptr > (void *)(all_k_mem + sizeof(all_k_mem))
+		(void *)b < (void *)all_k_mem ||
+		(void *)b > (void *)(all_k_mem + sizeof(all_k_mem))
 	) //check if the pointer is in all_k_mem, something... something fishy.
 		return;
 
-	struct block_t *b = ptr;
 	b->free = true;
 	joinFreeBlocks();
 }
+
 static void print_ruler(int n)
 {
 	const char hex[] = "123456789abcdef";
@@ -93,32 +94,47 @@ static void print_ruler(int n)
 static void print_block(struct block_t *b)
 {
 	printf("%s ",b->free ? "freeee!":"notfree");
-	printf("bytes: %lu ",b->size);
+	printf("bytes: %5.lu ",b->size);
+	printf("first/last byte: %c %c ", b->mem[0], b->mem[b->size - 1]);
 	printf("next: %p\n", b->next);
 }
 
 static void print_all_child_block(struct block_t *b)
 {
+	printf("\n");
 	while(b) {
 		print_block(b);
 		b = b->next;
 	}
+	printf("\n");
+}
+char *test(char ch, size_t len)
+{
+	char *test = mymalloc(sizeof(*test) * (len + 1));
+	print_all_child_block(head);
+	int i = 0;
+	while(i < len) {
+		test[i] = ch;
+		++i;
+	}
+	test[i] = '\0';
+	print_ruler(len);
+	printf("%s\n", test);
+	return test;
 }
 
 void main(void)
 {
-	const int n = 50;
+	const int t_a_bound = 50;
 	init();
 	print_all_child_block(head);
-	printf("\n");
-	char *test = mymalloc(sizeof(*test) * (n + 1));
-	print_all_child_block(head);
-	int i = 0;
-	while(i < n) {
-		test[i] = 'a';
-		++i;
-	}
-	test[i] = '\0';
-	print_ruler(n);
-	printf("%s\n", test);
+	char *a = test('a', 50);
+	char *b = test('b', 53);
+	myfree(a);
+	char *c = test('c', 32);
+	char *d = test('d', 32);
+	myfree(b);
+	char *e = test('e', 32);
+	char *f = test('f', 32);
+
 }
